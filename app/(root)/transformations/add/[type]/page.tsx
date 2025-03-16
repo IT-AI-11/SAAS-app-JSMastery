32db9af37cf5e89aefce8b914da10164e07cbd26
Nextjs <tech.united.it@gmail.com>
2025-03-16 22:47:11 +0200

    NO errors so far, types for Header.tsx added

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -5,11 +5,14 @@
 import React from 'react'
 import Header from '@/components/shared/Header'
 
+// it is the page: app/(root)/transformations/add/[type]/page.tsx
 export default function AddTransformationTypePage() {
   return (
     <>
-      <h1>  Add Transformation Type Page  </h1>
-      <Header />
+        <Header 
+        title="Transformation Title"
+        subtitle="Transformation SubTitle"
+      />
     </>
   )
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,8 +4,14 @@
 
 import React from 'react'
 
-export default function Header() {
+// to app/(root)/transformations/add/[type]/page.tsx
+const Header = ({ title, subtitle }: { title: string, subtitle?: string }) => {// типы для TypeScript прям в этой строке
   return (
-  <h1>header component</h1>
+    <>
+      <h2 className="h2-bold text-dark-600">{title}</h2>
+      {subtitle && <p className="p-16-regular mt-4">{subtitle}</p>}
+    </>
   )
 }
+
+export default Header
