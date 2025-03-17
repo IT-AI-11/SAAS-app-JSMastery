4eb6db0344cfa4dcfbbbe792e93756258a9e3f98
Nextjs <tech.united.it@gmail.com>
2025-03-17 08:59:53 +0200

    ERROR

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -5,9 +5,11 @@
 import React from 'react'
 import Header from '@/components/shared/Header'
 import { transformationTypes } from '@/constants'// см. [массив] transformationTypes
+import { NextPage } from 'next';//for testing
 
 // это страница: app/(root)/transformations/add/[type]/page.tsx
-export default function AddTransformationTypePage({ params: { type } }: SearchParamProps) {
+//export default function AddTransformationTypePage({ params: { type } }: SearchParamProps) {
+  const AddTransformationTypePage: NextPage<SearchParamProps> = ({ params: { type } }) => {//for testing
 //export default function AddTransformationTypePage() {
 
   // см. [массив] transformationTypes
@@ -28,3 +30,4 @@ export default function AddTransformationTypePage({ params: { type } }: SearchPa
     </>
   )
 }
+export default AddTransformationTypePage;//for testing
