8275410fe723fa9ffed5a63be0c05085e493cbaa
Nextjs <tech.united.it@gmail.com>
2025-03-17 08:53:49 +0200

    ERROR in here: ({ params: { type } }: SearchParamProps)

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -7,22 +7,22 @@ import Header from '@/components/shared/Header'
 import { transformationTypes } from '@/constants'// см. [массив] transformationTypes
 
 // это страница: app/(root)/transformations/add/[type]/page.tsx
-//export default function AddTransformationTypePage({ params: { type } }: SearchParamProps) {    original
-export default function AddTransformationTypePage() {
+export default function AddTransformationTypePage({ params: { type } }: SearchParamProps) {
+//export default function AddTransformationTypePage() {
 
   // см. [массив] transformationTypes
-  //const transformation = transformationTypes[type];
+  const transformation = transformationTypes[type];
 
   return (
     <>
 
       <Header
         // original
-        // title={transformation.title}
-        // subtitle={transformation.subTitle}
+         title={transformation.title}
+         subtitle={transformation.subTitle}
 
-        title="title test"
-        subtitle="subtitle test"
+        //title="title test"
+        //subtitle="subtitle test"
       />
 
     </>
